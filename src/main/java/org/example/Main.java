ed43639761c87785ba9be5fca7d86ed2eded4833
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-05-15 09:57:44 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,22 @@
 package org.example;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+public class Main extends Application {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+    @Override
+    public void start(Stage stage) {
+        Button boton = new Button("Haz clic aquí");
+
+        boton.setOnAction(event -> System.out.println("Boton pulsado"));
+
+        stage.setScene(new Scene(boton, 300, 200));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
